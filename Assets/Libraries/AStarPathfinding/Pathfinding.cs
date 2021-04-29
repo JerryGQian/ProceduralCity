6f167341f278c07c8b0c1b3ffb80c5c94e910bac
Jerry Qian <jerry@qian.ninja>
2021-04-29 00:05:45 -0400

    cleanup

--- Assets/Libraries/AStarPathfinding/Pathfinding.cs
+++ Assets/Libraries/AStarPathfinding/Pathfinding.cs
@@ -1,16 +1,4 @@
-﻿/* 
-    ------------------- Code Monkey -------------------
-
-    Thank you for downloading this package
-    I hope you find it useful in your projects
-    If you have any questions let me know
-    Cheers!
-
-               unitycodemonkey.com
-    --------------------------------------------------
- */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
